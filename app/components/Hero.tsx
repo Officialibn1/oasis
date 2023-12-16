77c775ffd23a3ce906bdc5408568089059dd754c
ibn <officialibn001@gmail.com>
2023-12-16 04:09:37 +0100

    Modified oasis balls

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,44 @@
+"use client";
+
 import React from "react";
 import "@/app/components/styles/Hero.css";
+import { motion } from "framer-motion";
+import { BitcoinOasisBall, EthereumOasisBall } from "./ui/OasisBalls";
 
 const Hero = () => {
 	return (
 		<section>
 			<div className='sectionContainer'>
-				<header>
-					<h1 className='text-4xl font-bold text-center'>Oasis Hero Section</h1>
-				</header>
+				<motion.header
+					className='heroSection'
+					transition={{ delayChildren: 0.6 }}>
+					{/* each ball item */}
+					<BitcoinOasisBall
+						font={"text-[80px]"}
+						height={"h-48"}
+						width={"w-48"}
+						ini={-200}
+						ani={80}
+					/>
+
+					{/* each ball item */}
+					<EthereumOasisBall
+						font={"text-[50px]"}
+						height={"h-16"}
+						width={"w-16"}
+						ini={-70}
+						ani={350}
+					/>
+
+					{/* each ball item */}
+					<BitcoinOasisBall
+						font={"text-[45px]"}
+						height={"h-32"}
+						width={"w-32"}
+						ini={-180}
+						ani={180}
+					/>
+				</motion.header>
 			</div>
 		</section>
 	);
--- app/components/ui/OasisBalls.tsx
+++ app/components/ui/OasisBalls.tsx
@@ -1,17 +1,74 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import { IoLogoBitcoin } from "react-icons/io5";
+import { LiaEthereum } from "react-icons/lia";
+import { TbBrandBinance } from "react-icons/tb";
+import { GiBlackHoleBolas } from "react-icons/gi";
+import "@/app/components/styles/OasisBalls.css";
 
-export function BitcoinOasisBall() {
-	return <div>BitcoinOasisBall</div>;
-}
+type BallProp = {
+	font: string;
+	height: string;
+	width: string;
+	ini: number;
+	ani: number;
+};
 
-export function EthereumOasisBall() {
-	return <div>EthereumOasisBall</div>;
-}
+export const BitcoinOasisBall = ({
+	font,
+	height,
+	width,
+	ini,
+	ani,
+}: BallProp) => {
+	return (
+		<motion.div
+			className={`bitcoinOasisBall ${height} ${width} `}
+			initial={{ x: ini }}
+			animate={{ x: ani }}
+			transition={{ duration: 0.6, ease: "linear" }}>
+			<IoLogoBitcoin className={font} />
+		</motion.div>
+	);
+};
 
-export function BNBOasisBall() {
-	return <div>BNBOasisBall</div>;
-}
+export const EthereumOasisBall = ({
+	font,
+	height,
+	width,
+	ini,
+	ani,
+}: BallProp) => {
+	return (
+		<motion.div
+			className={`ethereumOasisBall ${height} ${width} `}
+			initial={{ x: ini, opacity: 0 }}
+			animate={{ x: ani, opacity: 1 }}
+			transition={{ duration: 0.6, ease: "linear" }}>
+			<LiaEthereum />
+		</motion.div>
+	);
+};
 
-export function TetherOasisBall() {
-	return <div>TetherOasisBall</div>;
-}
+export const BNBOasisBall = ({ font, height, width, ini, ani }: BallProp) => {
+	return (
+		<motion.div className='bNBOasisBall'>
+			<TbBrandBinance />
+		</motion.div>
+	);
+};
+
+export const TetherOasisBall = ({
+	font,
+	height,
+	width,
+	ini,
+	ani,
+}: BallProp) => {
+	return (
+		<motion.div className='tetherOasisBall'>
+			<GiBlackHoleBolas />
+		</motion.div>
+	);
+};
